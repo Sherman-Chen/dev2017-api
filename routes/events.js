516141d09e8d0227281f75f56af4151a97498fda
Andrew <anxsaiyuki@gmail.com>
2017-02-11 21:42:46 -0800

    change callback

--- routes/events.js
+++ routes/events.js
@@ -7,7 +7,7 @@ var passport = require('passport');
 passport.use(new GoogleStrategy({
     clientID: '32013397026-5am1ufgrvlvkeh9kril5tgavdbc537mj.apps.googleusercontent.com',
     clientSecret: '1LLed7oPae0Da47Pw1RinLBX',
-    callbackURL: "http://localhost:8080/events/auth/google/callback"
+    callbackURL: "https://tranquil-wave-95184.herokuapp.com/"
   },
   function(token, tokenSecret, profile, done) {
     console.log('===================')
