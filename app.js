0012fe8547a0aea0f35c44a502c4ce2a6bb8c13f
Cyril <loki0119@gmail.com>
2017-02-12 00:59:00 -0800

    fix bugs

--- app.js
+++ app.js
@@ -65,8 +65,6 @@ app.get('/events/auth/google/callback',
   }
 );
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/events.js
+++ routes/events.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var rp = require('request-promise');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var passport = require('passport');
-
 var memo = "this is a message for Andrew at 11:00";
+
+var gcal = require('google-calendar');
+var google_calendar = new gcal.GoogleCalendar('ya29.GlvwA6SMEkFqyBMTvNmisEdLnfYYykdfTcr3BBGyg3KZ_jkP9zzrJWvbmr9X3HQzj9SqT8APsK9-LItJbMUjISOPjzygVQenAMEMz7hASxBP4O3Lx1SB3bLcSttR');
+var userToken = ''
 
 passport.use(new GoogleStrategy({
     clientID: '32013397026-5am1ufgrvlvkeh9kril5tgavdbc537mj.apps.googleusercontent.com',
@@ -12,28 +15,16 @@ passport.use(new GoogleStrategy({
     callbackURL: "https://secret-wave-94862.herokuapp.com//events/auth/google/callback"
   },
   function(token, tokenSecret, profile, done) {
-    console.log('===================')
+    console.log(token)
     return done(null, profile)
   }
 ));
-
-var gcal = require('google-calendar');
-var google_calendar = new gcal.GoogleCalendar('ya29.GlvwA6SMEkFqyBMTvNmisEdLnfYYykdfTcr3BBGyg3KZ_jkP9zzrJWvbmr9X3HQzj9SqT8APsK9-LItJbMUjISOPjzygVQenAMEMz7hASxBP4O3Lx1SB3bLcSttR');
-var userToken = ''
-
 
 /* GET home page. */
 router.post('/', function(req, res) {
   res.sendStatus(200)
   userToken = req.body.userToken
   console.log(userToken)
-  return
-});
-
-router.get('/widget', function(req, res) {
-  res.render('index', { title: 'Flock' });
-  console.log(req.query.flockEventToken)
-  console.log(req.query.flockEvent)
   return
 });
 
@@ -49,8 +40,6 @@ router.post('/gcl', function(req, res) {
     var event = {
       'summary': summary,
       'start': {
-        // 'dateTime': '2017-02-14T09:00:00-07:00',
-        // 'timeZone': 'America/Los_Angeles'
         'dateTime': start,
         'timeZone': tz
       },
@@ -66,23 +55,17 @@ router.post('/gcl', function(req, res) {
       console.log(data)
       res.send(200, calendarList)
     })
-
-    // google_calendar.events.quickAdd('cyrilyu.tw@gmail.com', 'Testing Again', function(err, data) {
-    //   if(err) return res.send(500,err);
-    //   console.log(data)
-    // });
-    
   })
+})
 
-  router.get('/redirect', function(req, res) {
-    console.log("===============" + req.query.flockEvent);
-    res.redirect('https://dev-ragingoctopus.herokuapp.com/');
-  })
+router.get('/redirect', function(req, res) {
+  console.log("===============" + req.query.flockEvent);
+  res.redirect('https://dev-ragingoctopus.herokuapp.com/');
+})
 
-  router.get('/getData', function(req, res) {
-
-    res.send(200, {"data": memo})
-    return
-  })
+router.get('/getData', function(req, res) {
+  res.send(200, {"data": memo})
+  return
+})
 
 module.exports = router;
