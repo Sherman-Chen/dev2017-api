0bd9c7c99e2f4a9fde0896018ae79f849a5ce453
Andrew <anxsaiyuki@gmail.com>
2017-02-12 01:22:41 -0800

    bug

--- routes/events.js
+++ routes/events.js
@@ -65,7 +65,8 @@ router.get('/redirect', function(req, res) {
   console.log("===============" + req.query.flockEvent);
   chat = req.query.flockEvent.chat;
   messageUid = req.query.flockEvent.messageUids.messageUid
-
+  console.log("===============chat" + chat);
+  console.log("===============messageUid" + messageUid);
   res.redirect('https://dev-ragingoctopus.herokuapp.com/');
 })
 
