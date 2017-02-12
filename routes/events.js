981eecb1f2f0fd5db35c0a53e39f785de8d5f9d9
Andrew <anxsaiyuki@gmail.com>
2017-02-12 01:41:31 -0800

    bug

--- routes/events.js
+++ routes/events.js
@@ -79,7 +79,7 @@ router.get('/getData', function(req, res) {
     form: {
       "token": userToken,
       "chat": chat,
-      "uids[]": messageUid
+      "uids": ["1486890669297-7E79Ap-mh104"]
     }
   }, function (e, r, body) {
     console.log("++++++++" + body);
